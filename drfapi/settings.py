dd128e147e1ebe72ce094355056cc74958366de4
daisuke_mac <daisukekawakami53@icloud.com>
2021-07-17 02:06:57 +0900

    mysqlclear

--- drfapi/settings.py
+++ drfapi/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'drfapi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django-db',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'db',
+        'PORT': '3306'
     }
 }
 
